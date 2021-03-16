8da2246d80496579a0142b03855ffe486f41825f
jsurville <julien.surville@gmail.com>
2021-03-16 21:50:10 +0100

    Add EntityFramwork, DbContext and connectionString; changed to Price Entity

---
+++ MyToDo.Models/Price.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyToApp.Models
+{
+    public class Price
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        [Required]
+        public string Currency { get; set; }
+
+        [Required]
+        public DateTime Creation_Time { get; set; }
+
+        [Required]
+        public DateTime Modification_Time { get; set; }
+    }
+}
--- MyToDoAPI/Resources/PriceResource.cs
+++ MyToDoAPI/Resources/PriceResource.cs
@@ -1,5 +1,6 @@
-﻿using System.Runtime.Serialization;
-using MyToDoAPI.Models;
+﻿using System;
+using System.Runtime.Serialization;
+using MyToApp.Models;
 
 namespace MyToDoAPI.Resources
 {
@@ -10,13 +11,24 @@ namespace MyToDoAPI.Resources
         public int Id { get; set; }
 
         [DataMember(Name = "value")]
-        public string Value { get; set; }
+        public decimal Value { get; set; }
+
+        [DataMember(Name = "currency")]
+        public string Currency { get; set; }
+
+        [DataMember(Name = "creation_time")]
+        public DateTime Creation_Time { get; set; }
+
+        [DataMember(Name = "modification_time")]
+        public DateTime Modification_Time { get; set; }
 
         public PriceResource(Price price)
         {
             Id = price.Id;
             Value = price.Value;
+            Currency = price.Currency;            
+            Creation_Time = price.Creation_Time;
+            Modification_Time = price.Modification_Time;
         }
-
     }
 }
